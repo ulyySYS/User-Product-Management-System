a5c42a9b28857d811dbad0e026da5e1f976146c6
Ulysses Grant Audan <>
2024-12-29 14:42:02 +0800

    Added "add" and "view" functionality, added GUI, integrated inventory to the GUI

--- src/main/java/org/example/Inventory.java
+++ src/main/java/org/example/Inventory.java
@@ -3,5 +3,36 @@ package org.example;
 import java.util.Scanner;
 
 public class Inventory {
+    static int storageCapacity;
+    static int tempCount = 0;
+    static Product[] tempDB;
 
+    Inventory(int storageCapacity){
+        this.storageCapacity = storageCapacity;
+        tempDB = new Product[storageCapacity];
+    }
+
+    public static void add(String name, double price, int quantity){
+        System.out.println(tempCount+ " " +storageCapacity);
+        if(tempCount < storageCapacity){
+
+//            Scanner myObj = new Scanner(System.in);
+//            System.out.println("Enter product name: ");
+//            String name = myObj.nextLine();
+
+            tempDB[tempCount] = new Product(name, price, quantity);
+
+            tempCount++;
+        } else {
+            System.out.println("full capacity");
+        }
+    }
+
+
+    public static void read(){
+        for(int i = 0; i < tempCount; i++){
+            System.out.println(tempDB[i].name);
+        }
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
 package org.example;
 
 import java.util.Scanner;
+import javax.swing.JFrame;
 
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        boolean exit = false;
+        Inventory tempInventory = new Inventory(10);
+        
+        
+        SystemGUI frame = new SystemGUI(tempInventory);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        System.out.println("exiting....");
     }
 }
